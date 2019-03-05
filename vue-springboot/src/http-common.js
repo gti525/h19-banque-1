c15f4d607528824d0eddd336ea3ce9f077e6acfe
Jefferson <jeffersonbrutus348@gmail.com>
2019-03-05 14:11:33 -0500

    Patch pour fonctionner sur la backend heroku

--- vue-springboot/src/http-common.js
+++ vue-springboot/src/http-common.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://banque1-backend-new.herokuapp.com/api",
+  //baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
     "Authorization": "Bearer " + localStorage.token,
   }
 });
-//baseURL: "https://banque1-backend.herokuapp.com/api",
+
