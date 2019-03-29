4461bdfbda3b3014d330a5b22f8103eceb03640f
Sleiman <sleiman.al-akhras.1@ens.etsmtl.ca>
2019-03-29 17:12:10 -0400

    Jeff_3 to Sleiman Merge

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://banque1-backend-new.herokuapp.com/api",
+  baseURL: "https://banque1-test.herokuapp.com/api",
   //baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
