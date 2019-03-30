f97f02433c57abc062b9b6ae43539c440016e1e6
Jordan <jordan.lam-deschamps.1@etsmtl.net>
2019-03-29 20:09:37 -0400

    Reset Password opérationnel!
    Il est désormais possible de réinitialiser le mot de passe via la page Login. -Jordan & Sleiman

--- src/components/ResetPassword.vue
+++ src/components/ResetPassword.vue
@@ -35,16 +35,17 @@
                     >
                 </div>
                 <div class="form-group">
-                    <label for="password">Mot de passe</label>
+                    <label for="password">Nouveau mot de passe: </label>
+                    <label for="message">*Ce dernier doit comporter au minimum 8 caractères, 1 majuscule, 1 chiffre et 1 caractère spécial.</label>
                     <input id="password"
-                           type="password"
+                           type="text"
                            v-model="password"
                            name="password"
                            class="form-control"
                     >
                 </div>
                 <div class="clearfix ">
-                    <button class="btn btn-primary btn-common float-right" v-on:click="resetPassword">Envoyer</button>
+                    <button class="btn btn-primary btn-common float-right" v-on:click="resetPassword">Réinitialiser</button>
                 </div>
             </div>
         </div>
@@ -90,15 +91,19 @@
                     .post("/auth/forgetPassword", { username: this.username, password: this.password,
                         question1: this.question1, question2: this.question2, answer1: this.answer1, answer2: this.answer2})
                     .then(request => this.modificationSuccessful(request))
-                    .catch(() => this.modificationFailed())
+                    .catch((e) => this.modificationFailed(e))
             },
             modificationSuccessful (req) {
+                alert("Le mot de passe a été changé avec succès.")
                 console.log(req)
                 this.$router.push('/')
             },
-            modificationFailed () {
-                this.$router.push('/errorPage')
-                delete localStorage.token
+            modificationFailed (e) {
+                alert("Une des réponses est éronnée. Veuillez réessayer.")
+                console.log(e);
+                console.log(e.request)
+                console.log(e.config)
+                console.log(e.message)
             }
         }
     };
