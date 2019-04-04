45db189f6f599339068ef885ec8c4bd9b49e3eb7
Cookie <francis.prud@gmail.com>
2019-04-04 16:34:22 -0400

    Banquo Uno enlevé du titre de CreditCardPayment

--- src/components/CreditCardPayment.vue
+++ src/components/CreditCardPayment.vue
@@ -1,12 +1,9 @@
 <template>
     <div>
         <nav-bar></nav-bar>
+        <h2>Paiement de la carte de crédit</h2>
         <div class="container">
-            <div class="app-title">Banquo Uno</div>
             <div class="transfer-container">
-                <div class="main-header">
-                    <h2>Paiement de la carte de crédit</h2>
-                </div>
                 <div>
                     <table class="table">
                         <tr>
